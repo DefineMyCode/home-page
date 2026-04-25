d39d6a26718698efd29d90e00a9e424539f91912
Dcsgo <dcsgomail@163.com>
2026-04-25 14:43:32 +0800

    自定义域名修改

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://your-domain.pages.dev',
+  site: 'https://dcsgo.com.cn',
   output: 'static',
   build: {
     assets: '_assets',
